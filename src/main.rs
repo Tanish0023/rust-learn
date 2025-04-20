a87315d361a3de71c927462e35f0715894ac73d0
Tanish0023 <tanish23aggarwal@gmail.com>
2025-04-20 21:45:41 +0530

    Week 26 end

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
 
-    let ans = sum(1,2);
-    println!("{}",ans);
+    let s = sum(10,20);
+    let name = String::from("Hello");
+    let v = vec![1,2,3];
+    let a = 10;
+    
+    assert_eq!(a, 10);
+    // let ans = sum(1,2);
+    println!("{}, {}, {:?}, {}",s,name,v,a);
 }
 
 fn sum(a: u32, b:u32) -> u32 {
